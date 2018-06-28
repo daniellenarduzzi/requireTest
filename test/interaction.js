c77d1f61bbd82d28517f20352d224559b6d20bba
Unknown <daniellenarduzzi@gmail.com>
2018-06-28 12:18:15 -0300

    finishing test

--- test/interaction.js
+++ test/interaction.js
@@ -3,18 +3,21 @@ const test = artifacts.require('test')
 contract('otherContract', addresses => {
   console.log('comienza test')
   const owner = addresses[0]
+  let instanceotherContract
+  let instancetest
   before(async () => {
      instanceotherContract = await otherContract.new( {from: owner})
-     instancetest = await test
-        .new(instanceotherContract.address ,{from: owner})
+     instancetest = await test.new(instanceotherContract.address ,{from: owner})
   })
 
   describe('testing interaction contract', () => {
 
     it('cant use chanA', async () => {
       try {
+        let a_before = await instanceotherContract.a()
         let x = await instancetest.chanA(2)
-        console.log(x);
+        let a_after = await instanceotherContract.a()
+        assert.equal(a_before,a_after)
         assert.fail()
       }
       catch(error) {
